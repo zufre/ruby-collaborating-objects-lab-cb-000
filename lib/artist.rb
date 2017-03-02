b7ea9db93d4905205410e93175a0ed52024e4fd4
Cedric David <cedric.david@btopenworld.com>
2017-03-02 06:20:42 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,43 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
+
+
+  @@all =[]
+
   def initialize(name)
 
     @name = name
-    
+    @songs = []
+    @@all << self
+
   end
+  def add_song(song)
+
+    @songs << song
+
+  end
+  def self.all
+
+    @@all
+  end
+
+  
+  def save
+    @@all << self
+  end
+  def self.find_or_create_by_name(name)
+    if  @@all.detect {|artist| artist.name == name} == nil
+      artist = self.new(name)
+      artist.name = name
+      artist
+    else
+      @@all.detect {|artist| artist.name == name}
+    end
+
+  end
+  def print_songs
+    @songs.each {|song| puts song.name}
+
+  end
+
 end
