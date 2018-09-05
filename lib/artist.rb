14b1593fa861f7aee1e3ca083c20c1e9e8b0f7c5
Cedric David <cedric.david@btopenworld.com>
2018-09-05 04:26:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,10 @@
 class Artist
 
-  attr_accessor :name :song
+  attr_accessor :name :songs
   def initialize(name)
     @name = name
     @songs = []
   end
 
-  def songs
-    @songs
-  end
+  
 end
